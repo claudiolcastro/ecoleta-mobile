8a35b163c1bc7aefb919a43f95104f83fcb08994
Claudio Castro <claudioluiz.castro@gmail.com>
2020-06-08 23:02:19 -0300

    use image_url

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -14,6 +14,7 @@ interface RouteParams {
 interface Data {
   point: {
     image: string,
+    image_url: string,
     name: string,
     email: string,
     whatsapp: string,
@@ -64,7 +65,7 @@ const Detail = () => {
           <Icon name="arrow-left" size={20} color="#34cb79" onPress={() => navigation.goBack()} />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
 
         <Text style={styles.pointName}>
          {data.point.name}
